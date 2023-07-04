059912c66863a8da24c161395870f6d33e1d1e53
BoKyoungChoi <bokyoung9693@gmail.com>
2023-07-04 10:38:27 +0900

    FEAT: adjust loader & delete ead

--- __init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-GMS_dir = os.path.join(current_dir, 'GMS')
-sys.path.append(GMS_dir)
--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from enum import Enum, unique
 
 MIRAE_NAME = "mirae-%s.csv"
@@ -43,3 +44,26 @@ class DataHelper:
 
         mirae_data = self.get_data(mirae_data_lists)
         return mirae_data
+
+
+class EDA(DataHelper):
+    def __init__(self):
+        super().__init__()
+        self.mirae_data = self.run_mirae()
+
+    @staticmethod
+    def get_histogram(df: pd.DataFrame) -> plt.plot:
+        num_columns = len(df.columns)
+
+        fig, axes = plt.subplots(nrows=num_columns,
+                                 ncols=1,
+                                 figsize=(8, num_columns*4))
+        for i, column in enumerate(df.columns):
+            axes[i].hist(df[column], bins=50,
+                         color='skyblue', edgecolor='black')
+            axes[i].set_title(column)
+            axes[i].set_xlabel("Value")
+            axes[i].set_ylabel("Frequency")
+
+        plt.tight_layout()
+        plt.show()
