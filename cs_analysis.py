f7edf7f0e53a85e989e624e54a8a8ff18a8b6fbb
BoKyoungChoi <bokyoung9693@gmail.com>
2023-07-24 23:18:10 +0900

    FEAT: Data preprocess clear with one hot encoding & uniting strings

--- cs_analysis.py
+++ cs_analysis.py
@@ -1,5 +1,9 @@
+import tensorflow
+
+from tensorflow import keras
+from keras.utils import to_categorical
 from sklearn.preprocessing import RobustScaler
-from sklearn.model_selection import train_test_split
+from itertools import chain
 
 from loader_cs import *
 from cs_learning import *
@@ -55,32 +59,123 @@ class CustomerAnalysis(CustomerHelper):
         res = pd.DataFrame(temp, columns=['Val', 'Elements'])
         return res
 
+    @staticmethod
+    def format_float_kr(num_str: str):
+        """
+        Change strings in KR with .0.
+        EX) 005935 & 5935.0: 5935.0 changes to 005935.
+        """
+        try:
+            num = float(num_str)
+            num_str = str(num)
+            ints, decs = num_str.split(".")
+            ints = ints.zfill(6)
+            return ints
+        except ValueError:
+            return num_str
+
+    @staticmethod
+    def format_float_hk(num_str: str):
+        """
+        Change strings in HK with 5 length & 0 in front.
+        EX) 00700 & 002594: 00700 changes to 0700.
+        """
+        try:
+            if len(num_str) == 5 and (num_str.startswith('00') or num_str.startswith('0')):
+                return num_str[1:]
+            else:
+                return num_str
+        except (TypeError, AttributeError):
+            return num_str
+
     @customer_classificer(param='ast')
-    def convert_string(self, item_type: str) -> dict:
-        # NOTE: ITM / high cost to replace.
-        # NOTE: Different space / vector & index cannot be used together.
+    def convert_float_kr(self, item_type: str):
+        cols = CustomerPool.dmst_ast_itm_rank.value
+        res = self.customer[cols].applymap(self.format_float_kr)
+        return res
 
+    @customer_classificer(param='ast')
+    def convert_float_hk(self, item_type: str):
+        cols = CustomerPool.ovst_ast_itm_rank.value
+        res = self.customer[cols].applymap(self.format_float_hk)
+        return res
+
+    @customer_classificer(param='trs')
+    def convert_float_trs(self, item_type: str):
+        flts = {'buy_trs_itm_rank': CustomerPool.buy_trs_itm_rank.value,
+                'sel_trs_itm_rank': CustomerPool.sel_trs_itm_rank.value}
+        cols = list(flts.values())
+        cols = list(chain(*cols))
+
+        res = self.customer[cols].applymap(self.format_float_kr)
+        res = res.applymap(self.format_float_hk)
+        return res
+
+    def convert_string_mkt(self):
         strs = {'dmst_ast_mkt_rank': CustomerPool.dmst_ast_mkt_rank.value,
-                'ovst_ast_mkt_rank': CustomerPool.ovst_ast_mkt_rank.value}
+                'ovst_ast_mkt_rank': CustomerPool.ovst_ast_mkt_rank.value,
+                'buy_trs_mkt_rank': CustomerPool.buy_trs_mkt_rank.value,
+                'sel_trs_mkt_rank': CustomerPool.sel_trs_mkt_rank.value}
+        strs = list(strs.values())
+        strs = list(chain(*strs))
 
-        def converter(name: str) -> pd.DataFrame:
-            temp = self.customer_filter.filter(strs[name])
-            unique_df = pd.DataFrame(
-                set(temp.stack().values), columns=['unique_df'])
-            unique_df['unique_id'] = pd.factorize(
-                unique_df['unique_df'])[0] + 1
-            convert_log = dict(
-                zip(unique_df['unique_df'], unique_df['unique_id']))
-            res = temp.replace(convert_log)
-            return res, convert_log
+        customer = self.customer.filter(strs)
+        unique_df = pd.DataFrame(set(customer.stack().values),
+                                 columns=['unique_df']).sort_values(by='unique_df')
+        unique_df['unique_id'] = pd.factorize(unique_df['unique_df'])[0] + 1
+        one_hot = to_categorical(unique_df['unique_id'])
+        unique_df['one_hot'] = pd.DataFrame({'one_hot': one_hot.tolist()})
+        # unique_df['one_hot'] = unique_df['one_hot'].apply(lambda x: [x])
+        # convert_log = dict(zip(unique_df['unique_df'], unique_df['one_hot']))
 
-        log = []
-        for key in strs:
-            temp, convert_log = converter(key)
-            cols = strs[key]
-            self.customer_filter[cols] = temp[cols]
-            log.append(convert_log)
-        return log
+        convert_log = dict(zip(unique_df['unique_df'], unique_df['unique_id']))
+        res = customer.replace(convert_log)
+        return res, convert_log
+
+    def get_convert(self):
+        # NOTE: HIGH TIME-COST
+        res = self.customer.copy()
+
+        convert_float_kr = self.convert_float_kr('ast')
+        convert_float_hk = self.convert_float_hk('ast')
+        convert_float_trs = self.convert_float_trs('trs')
+        convert_string_mkt, log = self.convert_string_mkt()
+
+        res[convert_float_kr.columns] = convert_float_kr
+        res[convert_float_hk.columns] = convert_float_hk
+        res[convert_float_trs.columns] = convert_float_trs
+        res[convert_string_mkt.columns] = convert_string_mkt
+        res.replace({'nan': np.nan}, inplace=True)
+        return res, log
+
+    # @customer_classificer(param='ast')
+    # def convert_string(self, item_type: str) -> dict:
+    #     # NOTE: ITM / high cost to replace.
+    #     # NOTE: Different space / vector & index cannot be used together.
+
+    #     strs = {'dmst_ast_mkt_rank': CustomerPool.dmst_ast_mkt_rank.value,
+    #             'ovst_ast_mkt_rank': CustomerPool.ovst_ast_mkt_rank.value, }
+    #     # 'buy_trs_mkt_rank': CustomerPool.buy_trs_mkt_rank.value,
+    #     # 'sel_trs_mkt_rank': CustomerPool.sel_trs_mkt_rank.value}
+
+    #     def converter(name: str) -> pd.DataFrame:
+    #         temp = self.customer_filter.filter(strs[name])
+    #         unique_df = pd.DataFrame(
+    #             set(temp.stack().values), columns=['unique_df'])
+    #         unique_df['unique_id'] = pd.factorize(
+    #             unique_df['unique_df'])[0] + 1
+    #         convert_log = dict(
+    #             zip(unique_df['unique_df'], unique_df['unique_id']))
+    #         res = temp.replace(convert_log)
+    #         return res, convert_log
+
+    #     log = []
+    #     for key in strs:
+    #         temp, convert_log = converter(key)
+    #         cols = strs[key]
+    #         self.customer_filter[cols] = temp[cols]
+    #         log.append(convert_log)
+    #     return log
 
     @staticmethod
     def get_autoencoder(df: pd.DataFrame, latent_dim: int, num_epochs: int):
@@ -115,12 +210,9 @@ class CustomerAnalysis(CustomerHelper):
 
         df_scaled = pd.DataFrame(
             scaler.fit_transform(temp_ints), columns=temp_ints.columns)
-        # TODO: Is train, test required in autoencoder? Unsupervised learning?
-        # df_train, df_test = train_test_split(
-        #     df_scaled, test_size=0.2, random_state=42)
 
         df_encoded = self.get_autoencoder(df_scaled, 3, 100)[0]
         # TODO: K-means by KMeansClustering in cs_analysis.py
         # NOTE: Index does not change. Masking will be in progress by index.
         # NOTE: Use index to reform df including string datas.
-        return
+        return df_encoded
